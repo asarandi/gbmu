2bc9ac52ed1eeafb57623da3acc9cc4daa872f53
Alexandr Sarandi <sarandi.alex@gmail.com>
2019-05-08 01:43:44 -0700

    throttle

--- src/timers.c
+++ src/timers.c
@@ -1,4 +1,47 @@
 #include "gb.h"
+#include <SDL.h>
+
+void gb_delay()
+{
+    static struct timespec tp, tp0, tp1, last_tp;
+    static uint64_t clock_cycles, cc0, current_ms, last_ms;
+
+
+    if (clock_gettime(CLOCK_REALTIME, &tp))
+        return ;
+
+    if (tp.tv_sec != tp0.tv_sec)
+    {
+        printf("clock cycles per second = %u, sec = %u, nsec = %u\n", state->cycles - cc0, tp.tv_sec, tp.tv_nsec);
+        cc0 = state->cycles;
+        tp0.tv_sec = tp.tv_sec;
+    }
+
+    if (tp.tv_sec > last_tp.tv_sec)
+        last_ms = 0;
+
+    current_ms = tp.tv_nsec / 1000000;
+    if (current_ms <= last_ms)
+        return ;
+
+    uint64_t time_units = current_ms - last_ms;
+    uint64_t clk = state->cycles - clock_cycles;
+
+    if (clk <= time_units * 4194)
+        return ;
+/*
+    if (state->cycles < clock_cycles + 4194 + 2500)
+        return ;
+*/
+    last_ms = current_ms;
+    memcpy(&last_tp, &tp, sizeof(struct timespec));
+    clock_cycles = state->cycles;
+//    tp1.tv_sec = 0;
+//    tp1.tv_nsec = 1000000;
+//    nanosleep(&tp1, NULL);
+    SDL_Delay(1);
+}
+
 
 void    timers_update(uint8_t *gb_mem, t_state *state, int current_cycles)
 {
@@ -7,6 +50,8 @@ void    timers_update(uint8_t *gb_mem, t_state *state, int current_cycles)
     static  uint64_t    freq = 1024;
 
     (void)state;
+
+    gb_delay();
 
     div = (gb_mem[0xff04] << 8) | gb_mem[0xff03];
     div += current_cycles;
@@ -51,16 +96,4 @@ void    timers_update(uint8_t *gb_mem, t_state *state, int current_cycles)
             gb_mem[0xff0f] |= 4; 
         }
     }
-}
-
-void delay()
-{
-    struct timespec ts;
-    ts.tv_sec = 0;
-    ts.tv_nsec = 1000000000L / 30;
-
-    struct timespec tz;
-    tz.tv_sec = 0;
-    tz.tv_nsec = 1000000000L / 30;
-    (void)nanosleep(&ts, &tz);
 }
