3e1ef3fdcc59c0fff535889857f8b160de4688c6
Alexandr SARANDI <asarandi@student.42.us.org>
2019-05-20 16:47:02 -0700

    improved mbc1

--- src/mbc1.c
+++ src/mbc1.c
@@ -5,19 +5,10 @@
 #define RAM_ADDRESS 0xa000
 #define RAM_SIZE    0x2000
 
+uint8_t ramg, bank1 = 1, bank2, mode;
+
 static  char    *ramfile_name;
-static  uint8_t ram_banks[4][RAM_SIZE];
-static  uint8_t ram_bank_number = 0;
-
-uint8_t mbc1_ram_read_u8(uint16_t addr)
-{    
-    uint8_t *gb_mem = state->gameboy_memory;
-
-    if (state->ram_enabled)
-        return gb_mem[addr];
-    else
-        return 0xff;
-}
+static  uint8_t ram_banks[RAM_SIZE * 4];
 
 void    savefile_read(char *rom_file)
 {
@@ -49,7 +40,6 @@ void    savefile_read(char *rom_file)
         return ;
     }
     close(fd);
-    (void)memcpy(&gb_mem[RAM_ADDRESS], ram_banks, RAM_SIZE);
     printf("ramfile loaded\n");
 }
 void    savefile_write()
@@ -59,9 +49,8 @@ void    savefile_write()
 
     if (!is_savefile_enabled())
         return ;
-    (void)memcpy(&ram_banks[ram_bank_number], &gb_mem[RAM_ADDRESS], RAM_SIZE);
     size = (RAM_SIZE*4)-1;
-    while ((size >= 0) && (!ram_banks[0][size]))
+    while ((size >= 0) && (!ram_banks[size]))
         size--;
     size++;
     if (!size) return ; //game did not use ram?
@@ -80,63 +69,62 @@ void    mbc1_ram_write_u8(uint16_t addr, uint8_t data)
 {    
     uint8_t *gb_mem = state->gameboy_memory;
 
-    if (!state->ram_enabled)
+    if (ramg != 0x0a)
         return ;
-    gb_mem[addr] = data;
+    if (mode == 0)
+        ram_banks[addr & 0x1fff] = data;
+    if (mode == 1)
+        ram_banks[(bank2 << 13) + (addr & 0x1fff)] = data;
 }
 
-void    mbc1_handler(uint16_t addr, uint8_t data)
-{
-    
-/*  
-     00h = ROM Banking Mode (up to 8KByte RAM, 2MByte ROM) (default)
-     01h = RAM Banking Mode (up to 32KByte RAM, 512KByte ROM)    
-*/
-    static  bool    ram_init        = false;
-    static  uint8_t banking_mode    = 0;
-    static  uint8_t rom_number      = 1;
-    uint8_t *gb_mem                 = state->gameboy_memory;
+uint8_t mbc1_ram_read_u8(uint16_t addr)
+{    
+    uint8_t *gb_mem = state->gameboy_memory;
 
+    if (ramg != 0x0a)
+        return 0xff;    
+    if (mode == 1)
+        return ram_banks[(bank2 << 13) + (addr & 0x1fff)];
+    return ram_banks[addr & 0x1fff];
+}
+
+uint8_t     mbc1_rom_read_u8(uint16_t addr)
+{
+    uint8_t *f = state->file_contents;
+	size_t	idx;
+
+    if (addr <= 0x3fff) {
+        if (mode == 0)  return f[addr];
+        if (mode == 1) {
+			idx = (bank2 << 19) + addr;
+			idx &= state->file_size - 1;
+			return f[idx];
+		}
+    }
+    if ((addr >= 0x4000) && (addr <= 0x7fff)) {
+		idx = (((bank2 << 5) | bank1) << 14 ) | (addr & 0x3fff);
+		idx &= state->file_size - 1;
+        return f[idx];
+	}
+    return 0xff;
+}
+
+void        mbc1_handler(uint16_t addr, uint8_t data)
+{
+    uint8_t *gb_mem = state->gameboy_memory;
 
     state->ram_read_u8 = &mbc1_ram_read_u8;
     state->ram_write_u8 = &mbc1_ram_write_u8;
 
-    if (addr <= 0x1fff) {
-        state->ram_enabled = ((data & 0x0f) == 0x0a) ? true : false;
+    if (addr <= 0x1fff)
+        ramg = data & 0x0f;
+    if ((addr >= 0x2000) && (addr <= 0x3fff)) {
+        bank1 = data & 0x1f;
+        if (bank1 == 0)
+			bank1 = 1;
     }
-
-    if ((addr >= 0x2000) && (addr <= 0x3fff))       //rom bank number
-    {
-        data = (rom_number & 0x60) | (data & 0x1f);        
-        if ((data == 0) || (data == 0x60) || (data == 0x40) || (data == 0x20))
-            data |= 1;
-
-        if (data != rom_number) {
-//            printf("setting rom_number; current = %d, new = %d\n", rom_number, data);
-            rom_number = data;
-            (void)memcpy(&gb_mem[ROM_ADDRESS], &state->file_contents[data * ROM_SIZE], ROM_SIZE);            
-        }
-    }
-
-    if ((addr >= 0x4000) && (addr <= 0x5fff))       //ram bank number or rom idx upper bits (bits 5,6)
-    {
-        data &= 3;
-        if (banking_mode == 0) {
-            data = (data << 5) | (rom_number & 0x1f);     //call self with addr 0x2000 and updated rom_number ? XXX
-            return mbc1_handler(0x2000, data);
-        }
-        if ((ram_bank_number != data) || (!ram_init)) {
-            (void)memcpy(&ram_banks[ram_bank_number], &gb_mem[RAM_ADDRESS], RAM_SIZE);
-            ram_init = true;
-        }
-        ram_bank_number = data;
-        (void)memcpy(&gb_mem[RAM_ADDRESS], &ram_banks[ram_bank_number], RAM_SIZE);
-    }
-
-
-    if ((addr >= 0x6000) && (addr <= 0x7fff))       //banking mode select
-    {
-        banking_mode = data & 1;
-//        printf("setting banking mode to %d\n", banking_mode);
-    }
+    if ((addr >= 0x4000) && (addr <= 0x5fff))
+        bank2 = data & 3;
+    if ((addr >= 0x6000) && (addr <= 0x7fff))
+        mode = data & 1;
 }
