eaed60862bd95aa2b07f8a43e0f7f5e7c453b77d
Alexandr Sarandi <sarandi.alex@gmail.com>
2019-09-13 06:24:25 -0700

    cycle through render palettes with <Q>/<W>

--- include/gb.h
+++ include/gb.h
@@ -14,46 +14,6 @@
 #define BOOTROM_ENABLED     false
 #define GUI_SCALE_FACTOR    3
 
-//#define GUI_RENDER_COLORS    0xffffff, 0xaaaaaa, 0x555555, 0x000000      /* classic black and white */
-//#define GUI_RENDER_COLORS    0xebdd77, 0xa1bc00, 0x0d8833, 0x004333
-//#define GUI_RENDER_COLORS    0x9bbc0f, 0x8bac0f, 0x306230, 0x0f380f      /* wikipedia */
-//#define GUI_RENDER_COLORS    0xf8e3c4, 0xcc3495, 0x6b1fb1, 0x0b0630      /* lospec.com: spacehaze */
-//#define GUI_RENDER_COLORS    0xeefded, 0x9a7bbc, 0x2d757e, 0x001b2e      /* lospec.com: purpledawn */
-//#define GUI_RENDER_COLORS    0xe0f0e8, 0xa8c0b0, 0x507868, 0x183030      /* lospec.com: platinum */
-//#define GUI_RENDER_COLORS    0xd0d058, 0xa0a840, 0x708028, 0x405010      /* lospec.com: nostalgia */
-//#define GUI_RENDER_COLORS    0xf4fbd0, 0x68cf68, 0x1e9178, 0x05241f      /* lospec.com: lightgreen */
-//#define GUI_RENDER_COLORS    0xbeeb71, 0x6ab417, 0x376d03, 0x172808      /* lospec.com: harshgreens */
-//#define GUI_RENDER_COLORS    0xc4cfa1, 0x8b956d, 0x4d533c, 0x1f1f1f      /* lospec.com: dirtyboy */
-//#define GUI_RENDER_COLORS    0xeff9d6, 0xba5044, 0x7a1c4b, 0x1b0326      /* lospec.com: crimson */
-//#define GUI_RENDER_COLORS    0xd0f4f8, 0x70b0c0, 0x3c3468, 0x1c0820      /* lospec.com: blue-seni */
-//#define GUI_RENDER_COLORS    0x000000, 0x676767, 0xb6b6b6, 0xffffff      /* lospec.com: 2-bit-grayscale */
-//#define GUI_RENDER_COLORS    0x332c50, 0x46878f, 0x94e344, 0xe2f3e4      /* lospec.com: kirokaze-gameboy */
-//#define GUI_RENDER_COLORS    0x5a3921, 0x6b8c42, 0x7bc67b, 0xffffb5      /* lospec.com: links-awakening-sgb */
-//#define GUI_RENDER_COLORS    0x331e50, 0xa63725, 0xd68e49, 0xf7e7c6      /* lospec.com: nintendo-super-gameboy */
-//#define GUI_RENDER_COLORS    0x081820, 0x346856, 0x88c070, 0xe0f8d0      /* lospec.com: nintendo-gameboy-bgb */
-//#define GUI_RENDER_COLORS    0x181010, 0x84739c, 0xf7b58c, 0xffefff      /* lospec.com: pokemon-sgb */
-//#define GUI_RENDER_COLORS    0x2b2b26, 0x706b66, 0xa89f94, 0xe0dbcd      /* lospec.com: grafxkid-gameboy-pocket-gray */
-#define GUI_RENDER_COLORS    0xffe4c2, 0xdca456, 0xa9604c, 0x422936      /* lospec.com: gb-chocolate */
-//#define GUI_RENDER_COLORS    0x102533, 0x42678e, 0x6f9edf, 0xcecece      /* lospec.com: megaman-v-sgb */
-//#define GUI_RENDER_COLORS    0x7c3f58, 0xeb6b6f, 0xf9a875, 0xfff6d3      /* lospec.com: ice-cream-gb */
-//#define GUI_RENDER_COLORS    0x2d1b00, 0x1e606e, 0x5ab9a8, 0xc4f0c2      /* lospec.com: mist-gb */
-//#define GUI_RENDER_COLORS    0x2c2137, 0x764462, 0xedb4a1, 0xa96868      /* lospec.com: rustic-gb */
-//#define GUI_RENDER_COLORS    0x243137, 0x3f503f, 0x768448, 0xacb56b      /* lospec.com: nintendo-gameboy-arne */
-//#define GUI_RENDER_COLORS    0x65296c, 0xb76591, 0xf4b26b, 0xfff5dd      /* lospec.com: grapefruit */
-//#define GUI_RENDER_COLORS    0x2c2c96, 0x7733e7, 0xe78686, 0xf7bef7      /* lospec.com: kirby-sgb */
-//#define GUI_RENDER_COLORS    0x2e463d, 0x385d49, 0x577b46, 0x7e8416      /* lospec.com: nintendo-gameboy-black-zero */
-//#define GUI_RENDER_COLORS    0x202020, 0x5e6745, 0xaeba89, 0xe3eec0      /* lospec.com: andrade-gameboy */
-//#define GUI_RENDER_COLORS    0x1f1f1f, 0x4d533c, 0x8b956d, 0xc4cfa1      /* lospec.com: pj-gameboy */
-//#define GUI_RENDER_COLORS    0xe7e8f3, 0x8c83c3, 0x634d8f, 0x120b19      /* lospec.com: darkboy4 */
-//#define GUI_RENDER_COLORS    0x4c625a, 0x7b9278, 0xabc396, 0xdbf4b4      /* lospec.com: grafxkid-gameboy-pocket-green */
-//#define GUI_RENDER_COLORS    0x2c1700, 0x047e60, 0xb62558, 0xaedf1e      /* lospec.com: metroid-ii-sgb */
-//#define GUI_RENDER_COLORS    0x2c2137, 0x446176, 0x3fac95, 0xa1ef8c      /* lospec.com: nymph-gb */
-//#define GUI_RENDER_COLORS    0x1e0000, 0x9e0000, 0xf78e50, 0xcef7f7      /* lospec.com: kid-icarus-sgb */
-//#define GUI_RENDER_COLORS    0x004333, 0x0d8833, 0xa1bc00, 0xebdd77      /* lospec.com: gb-easy-greens */
-//#define GUI_RENDER_COLORS    0x33ccff, 0x2086fd, 0x57001a, 0xb2194c      /* lospec.com: gameboy-pop */
-//#define GUI_RENDER_COLORS    0x622e4c, 0x7550e8, 0x608fcf, 0x8be5ff      /* lospec.com: wish-gb */
-//#define GUI_RENDER_COLORS    0x000000, 0x11c600, 0xdfa677, 0xeff7b6      /* lospec.com: super-mario-land-2-sgb */
-
 #define ROM_ADDRESS 0x4000
 #define ROM_SIZE    0x4000
 #define RAM_ADDRESS 0xa000
@@ -96,7 +56,7 @@ typedef struct  s_state {
     int             screen_mask;
     char            *rom_file;
     char            *ram_file;    
-	size_t          file_size;    
+    size_t          file_size;
     uint8_t         *file_contents;
     uint8_t         ram_banks[RAM_SIZE * 16];
     unsigned int    interrupt_cycles;
--- src/gui.c
+++ src/gui.c
@@ -8,20 +8,58 @@ SDL_Window      *gui_window;
 SDL_Renderer    *gui_renderer;
 SDL_Texture     *gui_buffer;
 
-/*
- Bit 3 - P13 Input Down  or Start    (0=Pressed) (Read Only)
- Bit 2 - P12 Input Up    or Select   (0=Pressed) (Read Only)
- Bit 1 - P11 Input Left  or Button B (0=Pressed) (Read Only)
- Bit 0 - P10 Input Right or Button A (0=Pressed) (Read Only)
-*/
-
 uint32_t        game_controls[]     = {SDLK_DOWN, SDLK_UP, SDLK_LEFT, SDLK_RIGHT, SDLK_RETURN, SDLK_RSHIFT, SDLK_z, SDLK_x};
 uint32_t        num_game_controls   = sizeof(game_controls) / sizeof(uint32_t);
-uint32_t        gui_colors[]        = {GUI_RENDER_COLORS};        /*classic*/
-//uint32_t        gui_colors[] = {0xe6e6e6ff, 0xb3b3b3ff, 0x737373ff, 0x333333ff};        /*shades of grey*/
-//uint32_t        gui_colors[] = {0x9bbc0fff, 0x8bac0fff, 0x306230ff, 0x0f380fff};        /*shades of green*/
-//uint32_t        gui_colors[] = {0xebdd77ff, 0xa1bc00ff, 0x0d8833ff, 0x004333ff};        /*green-beige*/
 
+struct s_render_palette {
+    uint32_t colors[4];
+    char *name;         };
+
+struct s_render_palette render_palettes[] = {
+    {{0xffffff, 0xaaaaaa, 0x555555, 0x000000}, "classic"},
+    {{0xe6e6e6, 0xb3b3b3, 0x737373, 0x333333}, "greys"},
+    {{0xebdd77, 0xa1bc00, 0x0d8833, 0x004333}, "pea-soup"},
+    {{0x9bbc0f, 0x8bac0f, 0x306230, 0x0f380f}, "wikipedia"},
+    {{0xf8e3c4, 0xcc3495, 0x6b1fb1, 0x0b0630}, "spacehaze"},
+    {{0xeefded, 0x9a7bbc, 0x2d757e, 0x001b2e}, "purpledawn"},
+    {{0xe0f0e8, 0xa8c0b0, 0x507868, 0x183030}, "platinum"},
+    {{0xd0d058, 0xa0a840, 0x708028, 0x405010}, "nostalgia"},
+    {{0xf4fbd0, 0x68cf68, 0x1e9178, 0x05241f}, "lightgreen"},
+    {{0xbeeb71, 0x6ab417, 0x376d03, 0x172808}, "harshgreens"},
+    {{0xc4cfa1, 0x8b956d, 0x4d533c, 0x1f1f1f}, "dirtyboy"},
+    {{0xeff9d6, 0xba5044, 0x7a1c4b, 0x1b0326}, "crimson"},
+    {{0xd0f4f8, 0x70b0c0, 0x3c3468, 0x1c0820}, "blue-seni"},
+    {{0x000000, 0x676767, 0xb6b6b6, 0xffffff}, "2-bit-grayscale"},
+    {{0x332c50, 0x46878f, 0x94e344, 0xe2f3e4}, "kirokaze-gameboy"},
+    {{0x5a3921, 0x6b8c42, 0x7bc67b, 0xffffb5}, "links-awakening-sgb"},
+    {{0x331e50, 0xa63725, 0xd68e49, 0xf7e7c6}, "nintendo-super-gameboy"},
+    {{0x081820, 0x346856, 0x88c070, 0xe0f8d0}, "nintendo-gameboy-bgb"},
+    {{0x181010, 0x84739c, 0xf7b58c, 0xffefff}, "pokemon-sgb"},
+    {{0x2b2b26, 0x706b66, 0xa89f94, 0xe0dbcd}, "grafxkid-gameboy-pocket-gray"},
+    {{0xffe4c2, 0xdca456, 0xa9604c, 0x422936}, "gb-chocolate"},
+    {{0x102533, 0x42678e, 0x6f9edf, 0xcecece}, "megaman-v-sgb"},
+    {{0x7c3f58, 0xeb6b6f, 0xf9a875, 0xfff6d3}, "ice-cream-gb"},
+    {{0x2d1b00, 0x1e606e, 0x5ab9a8, 0xc4f0c2}, "mist-gb"},
+    {{0x2c2137, 0x764462, 0xedb4a1, 0xa96868}, "rustic-gb"},
+    {{0x243137, 0x3f503f, 0x768448, 0xacb56b}, "nintendo-gameboy-arne"},
+    {{0x65296c, 0xb76591, 0xf4b26b, 0xfff5dd}, "grapefruit"},
+    {{0x2c2c96, 0x7733e7, 0xe78686, 0xf7bef7}, "kirby-sgb"},
+    {{0x2e463d, 0x385d49, 0x577b46, 0x7e8416}, "nintendo-gameboy-black-zero"},
+    {{0x202020, 0x5e6745, 0xaeba89, 0xe3eec0}, "andrade-gameboy"},
+    {{0x1f1f1f, 0x4d533c, 0x8b956d, 0xc4cfa1}, "pj-gameboy"},
+    {{0xe7e8f3, 0x8c83c3, 0x634d8f, 0x120b19}, "darkboy4"},
+    {{0x4c625a, 0x7b9278, 0xabc396, 0xdbf4b4}, "grafxkid-gameboy-pocket-green"},
+    {{0x2c1700, 0x047e60, 0xb62558, 0xaedf1e}, "metroid-ii-sgb"},
+    {{0x2c2137, 0x446176, 0x3fac95, 0xa1ef8c}, "nymph-gb"},
+    {{0x1e0000, 0x9e0000, 0xf78e50, 0xcef7f7}, "kid-icarus-sgb"},
+    {{0x004333, 0x0d8833, 0xa1bc00, 0xebdd77}, "gb-easy-greens"},
+    {{0x33ccff, 0x2086fd, 0x57001a, 0xb2194c}, "gameboy-pop"},
+    {{0x622e4c, 0x7550e8, 0x608fcf, 0x8be5ff}, "wish-gb"},
+    {{0x000000, 0x11c600, 0xdfa677, 0xeff7b6}, "super-mario-land-2-sgb"}
+};
+
+static unsigned int render_palette_idx = 20;
+#define num_render_palettes (sizeof(render_palettes) / sizeof(struct s_render_palette))
 
 bool
 gui_init() {
@@ -68,7 +106,8 @@ gui_render() {
             idx = state->screen_buf[buf_y * 160 + buf_x];
             for (gui_y = buf_y * GUI_SCALE_FACTOR; gui_y < (buf_y + 1) * GUI_SCALE_FACTOR; gui_y++) {
                 for (gui_x = buf_x * GUI_SCALE_FACTOR; gui_x < (buf_x + 1) * GUI_SCALE_FACTOR; gui_x++) {
-                    pixels[gui_y * GUI_SCALE_FACTOR * WND_WIDTH + gui_x] = gui_colors[idx];
+                    pixels[gui_y * GUI_SCALE_FACTOR * WND_WIDTH + gui_x] =
+                    render_palettes[render_palette_idx % num_render_palettes].colors[idx];
                 }
             }
         }
@@ -102,6 +141,10 @@ gui_update() {
                 gui_set_button_states(event.key.keysym.sym, 1);
                 if (event.key.keysym.sym == SDLK_ESCAPE)
                     state->done = true;
+                if (event.key.keysym.sym == SDLK_q)
+                    render_palette_idx--;
+                if (event.key.keysym.sym == SDLK_w)
+                    render_palette_idx++;
                 break ;
             case SDL_KEYUP:
                 gui_set_button_states(event.key.keysym.sym, 0);
@@ -111,7 +154,6 @@ gui_update() {
     SDL_RenderClear(gui_renderer);
     SDL_RenderCopy(gui_renderer, gui_buffer, NULL, NULL);
     SDL_RenderPresent(gui_renderer);
-//    SDL_Delay(GUI_DELAY_DURATION);
 }
 
 void gb_throttle()
