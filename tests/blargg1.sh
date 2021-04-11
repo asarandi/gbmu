101d4064b7d141e8bda8153317e24c4943eaf7db
Alexandr SARANDI <sarandi.alex@gmail.com>
2021-04-10 23:41:54 -0700

    added blargg1 tests

---
+++ tests/blargg1.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+cd extras/roms/blargg-gb-tests/
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+if [ ! -f ../../../gbmu ]; then
+    echo "cannot find gbmu"
+    exit 1
+fi
+
+while IFS= read -r f; do
+    ../../../gbmu -t blargg1 "$f"
+    ret=$?
+    if [ $ret -eq 10 ]; then
+        echo "success $f"
+    elif [ $ret -eq 11 ]; then
+        echo "failure $f"
+    elif [ $ret -eq 12 ]; then
+        echo "timeout $f"
+    else
+        echo "other $ret $f"
+    fi
+done <<< "cpu_instrs/individual/01-special.gb
+cpu_instrs/individual/02-interrupts.gb
+cpu_instrs/individual/03-op sp,hl.gb
+cpu_instrs/individual/04-op r,imm.gb
+cpu_instrs/individual/05-op rp.gb
+cpu_instrs/individual/06-ld r,r.gb
+cpu_instrs/individual/07-jr,jp,call,ret,rst.gb
+cpu_instrs/individual/08-misc instrs.gb
+cpu_instrs/individual/09-op r,r.gb
+cpu_instrs/individual/10-bit ops.gb
+cpu_instrs/individual/11-op a,(hl).gb
+instr_timing/instr_timing.gb
+mem_timing/individual/01-read_timing.gb
+mem_timing/individual/02-write_timing.gb
+mem_timing/individual/03-modify_timing.gb"
--- tests/blargg2.sh
+++ tests/blargg2.sh
@@ -23,7 +23,8 @@ while IFS= read -r f; do
     else
         echo "other $ret $f"
     fi
-done <<< "cgb_sound/rom_singles/01-registers.gb
+done <<< "cgb_sound/cgb_sound.gb
+cgb_sound/rom_singles/01-registers.gb
 cgb_sound/rom_singles/02-len ctr.gb
 cgb_sound/rom_singles/03-trigger.gb
 cgb_sound/rom_singles/04-sweep.gb
@@ -35,6 +36,7 @@ cgb_sound/rom_singles/09-wave read while on.gb
 cgb_sound/rom_singles/10-wave trigger while on.gb
 cgb_sound/rom_singles/11-regs after power.gb
 cgb_sound/rom_singles/12-wave.gb
+dmg_sound/dmg_sound.gb
 dmg_sound/rom_singles/01-registers.gb
 dmg_sound/rom_singles/02-len ctr.gb
 dmg_sound/rom_singles/03-trigger.gb
@@ -47,9 +49,12 @@ dmg_sound/rom_singles/09-wave read while on.gb
 dmg_sound/rom_singles/10-wave trigger while on.gb
 dmg_sound/rom_singles/11-regs after power.gb
 dmg_sound/rom_singles/12-wave write while on.gb
+halt_bug.gb
+interrupt_time/interrupt_time.gb
 mem_timing-2/rom_singles/01-read_timing.gb
 mem_timing-2/rom_singles/02-write_timing.gb
 mem_timing-2/rom_singles/03-modify_timing.gb
+oam_bug/oam_bug.gb
 oam_bug/rom_singles/1-lcd_sync.gb
 oam_bug/rom_singles/2-causes.gb
 oam_bug/rom_singles/3-non_causes.gb
