8c618ccd4242ff2624941f91a8bb17ca37b64bfa
Alexandr SARANDI <sarandi.alex@gmail.com>
2021-04-18 12:55:47 -0700

    sound.c use mask for reading sound registers
    passes blargg dmg_sound 01-registers
    
    renamed test results files to csv

--- tests/acceptance.sh
+++ tests/acceptance.sh
@@ -2,24 +2,26 @@
 
 cd extras/roms/mooneye-gb_hwtests/
 if [ $? -ne 0 ]; then
+    echo "failed to change dir" >&2
     exit 1
 fi
 
 if [ ! -f ../../../gbmu ]; then
-    echo "cannot find gbmu"
+    echo "cannot find gbmu" >&2
     exit 1
 fi
 
+echo "result,file"
 for f in $(find acceptance/ -iname "*.gb" | sort); do
     ../../../gbmu -t mooneye "$f"
     ret=$?
     if [ $ret -eq 10 ]; then
-        echo "success $f"
+        echo "success,$f"
     elif [ $ret -eq 11 ]; then
-        echo "failure $f"
+        echo "failure,$f"
     elif [ $ret -eq 12 ]; then
-        echo "timeout $f"
+        echo "timeout,$f"
     else
-        echo "other $ret $f"
+        echo "other $ret,$f"
     fi
 done
--- tests/blargg1.sh
+++ tests/blargg1.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
 cd extras/roms/blargg-gb-tests/
-
 if [ $? -ne 0 ]; then
+    echo "failed to change dir" >&2
     exit 1
 fi
 
 if [ ! -f ../../../gbmu ]; then
-    echo "cannot find gbmu"
+    echo "cannot find gbmu" >&2
     exit 1
 fi
 
+echo "result,file"
 while IFS= read -r f; do
     ../../../gbmu -t blargg1 "$f"
     ret=$?
     if [ $ret -eq 10 ]; then
-        echo "success $f"
+        echo "success,$f"
     elif [ $ret -eq 11 ]; then
-        echo "failure $f"
+        echo "failure,$f"
     elif [ $ret -eq 12 ]; then
-        echo "timeout $f"
+        echo "timeout,$f"
     else
-        echo "other $ret $f"
+        echo "other $ret,$f"
     fi
 done <<< "cpu_instrs/cpu_instrs.gb
 cpu_instrs/individual/01-special.gb
