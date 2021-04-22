eb0cd72ae18d0bd07df449d8d1133c1e99dd0f88
Alexandr SARANDI <sarandi.alex@gmail.com>
2021-04-21 19:06:04 -0700

    joypad.c: formatting

--- src/joypad.c
+++ src/joypad.c
@@ -6,20 +6,22 @@ void joypad_request_interrupt() {
 }
 
 uint8_t joypad_read() {
-    uint8_t i, offset, bit, res;
-    res = gb_mem[rP1];
-    offset = (res & P1F_GET_BTN) == 0 ? 0 : 4;
+    uint8_t i, res;
+    res = gb_mem[rP1] & 0b00110000;
 
-    for (i = 0; i < 4; i++) {
-        bit = (state->buttons[i + offset] & 1) ^ 1;
-        res &= ~(1 << (3 - i));
-        res |= bit << (3 - i);
+    if (res == P1F_GET_DPAD) {
+        for (i = 0; i < 4; i++) {
+            res |= ((state->buttons[i] & 1) ^ 1) << (3 - i);
+        }
+    } else if (res == P1F_GET_BTN) {
+        for (i = 0; i < 4; i++) {
+            res |= ((state->buttons[i + 4] & 1) ^ 1) << (3 - i);
+        }
     }
 
-    gb_mem[rP1] = res;
-    return res;
+    return 0b11000000 | res;
 }
 
 void joypad_write(uint8_t data) {
-    gb_mem[rP1] = (gb_mem[rP1] & 15) | (data & 48);
+    gb_mem[rP1] = data & 0b00110000;
 }
