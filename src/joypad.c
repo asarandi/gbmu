8f5456958c5d98cbaa13c1936a7271e21b65ebf9
Alexandr SARANDI <sarandi.alex@gmail.com>
2021-04-30 19:20:02 -0700

    joypad.c proper

--- src/joypad.c
+++ src/joypad.c
@@ -7,19 +7,19 @@ void joypad_request_interrupt() {
 
 uint8_t joypad_read() {
     uint8_t i, res;
-    res = gb_mem[rP1] & 0b00110000;
+    res = (gb_mem[rP1] & 0x30) | (~0x30);
 
-    if (res == P1F_GET_DPAD) {
+    if ((res & 0x30) == P1F_GET_DPAD) {
         for (i = 0; i < 4; i++) {
-            res |= ((state->buttons[i] & 1) ^ 1) << (3 - i);
+            res ^= ((state->buttons[i] & 1) << (3 - i));
         }
-    } else if (res == P1F_GET_BTN) {
+    } else if ((res & 0x30) == P1F_GET_BTN) {
         for (i = 0; i < 4; i++) {
-            res |= ((state->buttons[i + 4] & 1) ^ 1) << (3 - i);
+            res ^= ((state->buttons[i + 4] & 1) << (3 - i));
         }
     }
 
-    return 0b11000000 | res;
+    return res;
 }
 
 void joypad_write(uint8_t data) {
