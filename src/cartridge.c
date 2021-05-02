592af52b397bb91ba78860276533528c1cd23f27
Alexandr SARANDI <sarandi.alex@gmail.com>
2021-05-02 00:13:34 -0700

    consolidate mbc 1/2/3/5 into cartridge.c

--- src/cartridge.c
+++ src/cartridge.c
@@ -1,6 +1,8 @@
 #include "gb.h"
 #include <errno.h>
 
+static uint8_t ramg, romb0 = 1, romb1, ramb, mode;
+
 #define IS_ROM_ONLY                       (gb_mem[0x0147] == 0x00 ? 1:0)
 #define IS_MBC1                           (gb_mem[0x0147] == 0x01 ? 1:0)
 #define IS_MBC1_RAM                       (gb_mem[0x0147] == 0x02 ? 1:0)
@@ -258,5 +260,240 @@ void cartridge_init() {
         state->ram_write_u8 = &mbc5_ram_write_u8;
         state->rom_read_u8 = &mbc5_rom_read_u8;
         state->rom_write_u8 = &mbc5_rom_write_u8;
+    }
+}
+
+void mbc1_ram_write_u8(uint16_t addr, uint8_t data) {
+    uint8_t *f = state->file_contents;
+
+    if (ramg != 0x0a) {
+        return;
+    }
+
+    if ((mode == 1) && (f[0x149] > 2)) {
+        state->ram_banks[(romb1 << 13) + (addr & 0x1fff)] = data;
+        return;
+    }
+
+    state->ram_banks[addr & 0x1fff] = data;
+}
+
+uint8_t mbc1_ram_read_u8(uint16_t addr) {
+    uint8_t *f = state->file_contents;
+
+    if (ramg != 0x0a) {
+        return 0xff;
+    }
+
+    if ((mode == 1) && (f[0x149] > 2)) {
+        return state->ram_banks[(romb1 << 13) + (addr & 0x1fff)];
+    }
+
+    return state->ram_banks[addr & 0x1fff];
+}
+
+uint8_t mbc1_rom_read_u8(uint16_t addr) {
+    uint8_t *f = state->file_contents;
+    int idx;
+
+    if (addr <= 0x3fff) {
+        if (mode == 0) {
+            return f[addr];
+        }
+
+        if (mode == 1) {
+            idx = (romb1 << 19) + addr;
+            idx &= state->file_size - 1;
+            return f[idx];
+        }
+    }
+
+    if ((addr >= 0x4000) && (addr <= 0x7fff)) {
+        idx = (((romb1 << 5) | romb0) << 14) | (addr & 0x3fff);
+        idx &= state->file_size - 1;
+        return f[idx];
+    }
+
+    return 0xff;
+}
+
+void mbc1_rom_write_u8(uint16_t addr, uint8_t data) {
+    if (addr <= 0x1fff) {
+        ramg = data & 0x0f;
+    }
+
+    if ((addr >= 0x2000) && (addr <= 0x3fff)) {
+        romb0 = data & 0x1f;
+
+        if (romb0 == 0) {
+            romb0 = 1;
+        }
+    }
+
+    if ((addr >= 0x4000) && (addr <= 0x5fff)) {
+        romb1 = data & 3;
+    }
+
+    if ((addr >= 0x6000) && (addr <= 0x7fff)) {
+        mode = data & 1;
+    }
+}
+
+void mbc2_ram_write_u8(uint16_t addr, uint8_t data) {
+    if (ramg != 0x0a) {
+        return;
+    }
+
+    state->ram_banks[addr & 0x1ff] = (data & 0x0f) | 0xf0;
+}
+
+uint8_t mbc2_ram_read_u8(uint16_t addr) {
+    if (ramg != 0x0a) {
+        return 0xff;
+    }
+
+    return (state->ram_banks[addr & 0x1ff] & 0x0f) | 0xf0;
+}
+
+uint8_t mbc2_rom_read_u8(uint16_t addr) {
+    uint8_t *f = state->file_contents;
+    int idx;
+
+    if (addr <= 0x3fff) {
+        return f[addr];
+    }
+
+    if ((addr >= 0x4000) && (addr <= 0x7fff)) {
+        idx = (romb0 << 14) | (addr & 0x3fff);
+        idx &= state->file_size - 1;
+        return f[idx];
+    }
+
+    return 0xff;
+}
+
+void mbc2_rom_write_u8(uint16_t addr, uint8_t data) {
+    if (addr <= 0x3fff) {
+        if (addr & 0x100) {
+            romb0 = data & 15;
+
+            if (romb0 == 0) {
+                romb0 = 1;
+            }
+        } else {
+            ramg = data & 15;
+        }
+    }
+}
+
+void mbc3_ram_write_u8(uint16_t addr, uint8_t data) {
+    if (ramg != 0x0a) {
+        return;
+    }
+
+    state->ram_banks[(ramb << 13) + (addr & 0x1fff)] = data;
+}
+
+uint8_t mbc3_ram_read_u8(uint16_t addr) {
+    if (ramg != 0x0a) {
+        return 0xff;
+    }
+
+    return state->ram_banks[(ramb << 13) + (addr & 0x1fff)];
+}
+
+uint8_t mbc3_rom_read_u8(uint16_t addr) {
+    uint8_t *f = state->file_contents;
+    int idx;
+
+    if (addr <= 0x3fff) {
+        return f[addr];
+    }
+
+    if ((addr >= 0x4000) && (addr <= 0x7fff)) {
+        idx = (romb0 << 14) | (addr & 0x3fff);
+        idx &= state->file_size - 1;
+        return f[idx];
+    }
+
+    return 0xff;
+}
+
+void mbc3_rom_write_u8(uint16_t addr, uint8_t data) {
+    if (addr <= 0x1fff) {
+        ramg = data;
+    }
+
+    if ((addr >= 0x2000) && (addr <= 0x3fff)) {
+        romb0 = data;
+
+        if (romb0 == 0) {
+            romb0 = 1;
+        }
+    }
+
+    if ((addr >= 0x4000) && (addr <= 0x5fff)) {
+        data &= 0x0f;
+
+        if (data <= 3) {
+            ramb = data & 3;
+        } else {
+            printf("mbc3 rtc register select, data = %02x\n", data);    /* TODO */
+        }
+    }
+
+    if ((addr >= 0x6000) && (addr <= 0x7fff)) {
+        printf("mbc3 latch clock data, data = %02x\n", data);    /* TODO */
+    }
+}
+
+void mbc5_ram_write_u8(uint16_t addr, uint8_t data) {
+    if (ramg != 0x0a) {
+        return;
+    }
+
+    state->ram_banks[(ramb << 13) + (addr & 0x1fff)] = data;
+}
+
+uint8_t mbc5_ram_read_u8(uint16_t addr) {
+    if (ramg != 0x0a) {
+        return 0xff;
+    }
+
+    return state->ram_banks[(ramb << 13) + (addr & 0x1fff)];
+}
+
+uint8_t mbc5_rom_read_u8(uint16_t addr) {
+    uint8_t *f = state->file_contents;
+    int idx;
+
+    if (addr <= 0x3fff) {
+        return f[addr];
+    }
+
+    if ((addr >= 0x4000) && (addr <= 0x7fff)) {
+        idx = (((romb1 << 8) | romb0) << 14) | (addr & 0x3fff);
+        idx &= state->file_size - 1;
+        return f[idx];
+    }
+
+    return 0xff;
+}
+
+void mbc5_rom_write_u8(uint16_t addr, uint8_t data) {
+    if (addr <= 0x1fff) {
+        ramg = data;
+    }
+
+    if ((addr >= 0x2000) && (addr <= 0x2fff)) {
+        romb0 = data;
+    }
+
+    if ((addr >= 0x3000) && (addr <= 0x3fff)) {
+        romb1 = data & 1;
+    }
+
+    if ((addr >= 0x4000) && (addr <= 0x5fff)) {
+        ramb = data & 15;
     }
 }
